1fcfbc21d905d420a09f22d48191e274e04cc4bd
cris <cristianm.px1995@gmail.com>
2025-03-23 11:39:52 -0300

    Loading page implemented

--- src/components/logo/logo.js
+++ src/components/logo/logo.js
@@ -2,12 +2,20 @@ import "./logo.css"
 
 const Logo = (() => {
 
+    const toggleSidebar = () => {
+        const el = document.querySelector('#SidebarLeft');
+        el.classList.add('hidding');
+        setTimeout(() => {
+            el.classList.remove('hidding');
+        }, 210);
+    }
+
     return <div id="Logo">
             <img id="LogoImg" src="logo.png"/>
             <h2 className="fontController">ORBET</h2>
             <div id="arrowWrapper">
                 <i className="orbeticon-arrowleft arrow"></i>
-                <i className="orbeticon-arrowleft arrow arrowInverted" ></i>
+                <i className="orbeticon-arrowleft arrow arrowInverted" onClick={toggleSidebar}></i>
             </div>
         </div>
 
